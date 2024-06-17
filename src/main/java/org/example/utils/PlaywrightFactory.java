5588b86f8fa9785829fe9ed5397d49c30a7d69dd
Gustavo Cardoso <gusstavocardoso@gmail.com>
2024-06-17 19:49:56 -0300

    set headldess mode true

--- src/main/java/org/example/utils/PlaywrightFactory.java
+++ src/main/java/org/example/utils/PlaywrightFactory.java
@@ -18,7 +18,7 @@ public class PlaywrightFactory {
     public static Page getPage() {
         if (playwright == null) {
             playwright = Playwright.create();
-            browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
             context = browser.newContext();
             page = context.newPage();
         }
